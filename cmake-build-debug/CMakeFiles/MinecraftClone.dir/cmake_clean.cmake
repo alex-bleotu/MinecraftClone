11120435dc5349be8f9439fe3a425ba7d506855e
AlexDavid26 <alexbleotu2006@gmail.com>
2024-09-16 12:56:58 +0300

    Trying to switch to OpenGL

---
+++ cmake-build-debug/CMakeFiles/MinecraftClone.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MinecraftClone.dir/main.cpp.obj"
+  "CMakeFiles/MinecraftClone.dir/main.cpp.obj.d"
+  "MinecraftClone.exe"
+  "MinecraftClone.exe.manifest"
+  "MinecraftClone.pdb"
+  "libMinecraftClone.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MinecraftClone.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
